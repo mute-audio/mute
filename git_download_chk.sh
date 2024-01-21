c7551b1926a6cae03c5f12789dba1e47a025950b
mute-audio <kitamura_design@me.com>
2024-01-21 19:36:34 +0900

    Download check script

---
+++ git_download_chk.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+curl --silent --header \
+"Accept: application/vnd.github.v3+json" \
+https://api.github.com/repos/mute-audio/mute/releases \
+| grep -e "\"name\"" -e "\"download_count\"" \
+| sed -e 's/ //g' -e 's/\"//g' -e 's/,//g' -e 's/name://g'
+
+exit 0
