91f6f26bcb3304475e8b56c2ebb17e21211ffd18
mute-audio <kitamura_design@me.com>
2024-01-23 19:35:23 +0900

    Revert "Revert "Update Download check script""
    This reverts commit 76e6f5b909f52b3cdda3ad1619347ecb74902de2.

--- git_download_chk.sh
+++ git_download_chk.sh
@@ -4,6 +4,6 @@ curl --silent --header \
 "Accept: application/vnd.github.v3+json" \
 https://api.github.com/repos/mute-audio/mute/releases \
 | grep -e "\"name\"" -e "\"download_count\"" \
-| sed -e 's/ //g' -e 's/\"//g' -e 's/,//g' -e 's/name://g'
+| sed -e 's/^ *//' -e 's/\"//g' -e 's/,//g' -e 's/name: //g'
 
 exit 0
