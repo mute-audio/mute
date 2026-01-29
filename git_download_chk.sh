c7455dba7b35bcade5cde647c079eaca2f0dd84a
mute-audio <kitamura_design@me.com>
2026-01-29 21:59:23 +0900

    update

--- git_download_chk.sh
+++ git_download_chk.sh
@@ -6,7 +6,7 @@
 
 curl --silent --header \
 "Accept: application/vnd.github.v3+json" \
-https://api.github.com/repos/mute-audio/mute/releases \
+https://api.github.com/repos/mute-audio/mute/releases/latest \
 | grep -e "\"name\"" -e "\"download_count\"" \
 | sed -e 's/^ *//' -e 's/\"//g' -e 's/,//g' -e 's/name: //g'
 
